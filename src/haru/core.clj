fa0ff46edf304391a6b3b2c1d5bb9dfc22e7a00e
taptap <alexey.e.egorov@gmail.com>
2017-09-02 16:39:22 +0300

    add functions for database creation/initialization and post addition
    Now we can init database, create connection, and insert new posts in
    the database.

--- src/haru/core.clj
+++ src/haru/core.clj
@@ -11,7 +11,43 @@
    [korma.core :refer :all]
    (:gen-class)))
 
+(defdb db (postgres {:db "haru"
+                     :user "haru"
+                     :password "12345"}))
 
+(defn init-db []
+  (let [schema
+        ["
+CREATE TABLE IF NOT EXISTS posts(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(64),
+  author VARCHAR(512),
+  text TEXT,
+  date TIMESTAMP WITHOUT TIME ZONE,
+  image TEXT,
+  parentid INTEGER);
+"]]
+    (map exec-raw schema)))
+
+(defentity posts
+  (pk :id))
+
+(defn db-save-post [& {:keys [title author text image parent-id]
+                       :or {title ""
+                            author "名無しさん"
+                            text ""}}]
+  (if (and (empty? text)
+           (nil? image))
+    (throw (java.lang.RuntimeException.
+            "Post must have either image or text")))
+  (insert posts
+          (values
+           {:title title
+            :author author
+            :text text
+            :date (java.sql.Timestamp. (.getTime (java.util.Date.)))
+            :image image
+            :parentid parent-id})))
 
 (defn comment-css []
   [:div.comment {:background-color (css-color/rgb 214 218 240)
